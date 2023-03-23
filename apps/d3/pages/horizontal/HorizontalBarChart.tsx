40c29c88a6c7d4c84d22960a8ff529dab59e85d3
lsw8168 <lsw8168@estsecurity.com>
2023-03-23 13:42:21 +0900

    HorizontalBarChart

---
+++ apps/d3/pages/horizontal/HorizontalBarChart.tsx
@@ -0,0 +1,206 @@
+import { useEffect, useCallback, useState, useMemo } from 'react';
+import {
+  axisBottom,
+  axisLeft,
+  scaleBand,
+  scaleLinear,
+  select,
+  max,
+  pointer,
+  scaleOrdinal,
+  schemePaired,
+} from 'd3';
+import { Data, Dimensions } from './index';
+
+interface IDonutChartProps {
+  dimensions: Dimensions;
+  data: Data[];
+  propertiesNames: string[];
+}
+
+const HorizontalBarChart = (props: IDonutChartProps) => {
+  const {
+    data,
+    dimensions: {
+      width,
+      height,
+      margin: { top: marginTop, right: marginRight, bottom: marginBottom, left: marginLeft },
+    },
+  } = props;
+
+  const [loaded, setLoaded] = useState(false);
+
+  const memoizedDrawCallback = useCallback(() => {
+    // resize가 안 일어나면 여기 안들어옴
+    // 데이터만 바뀌는 경우 여기 안들어옴
+    select('#wrapper').selectAll('rect').exit().remove();
+  }, []);
+
+  // const xScale = useMemo(
+  //   () =>
+  //     scaleBand()
+  //       // @ts-ignore
+  //       .domain(data.map((d) => d.name))
+  //       .round(true)
+  //       .rangeRound([marginLeft, width - marginRight])
+  //       .padding(0.2),
+  //   [data, width, marginLeft, marginRight],
+  // );
+
+  // const yScale = useMemo(
+  //   () =>
+  //     scaleLinear()
+  //       // @ts-ignore
+  //       .domain([0, Number(max(data, (d) => d.value)) + 10])
+  //       .range([height - marginBottom, marginTop]),
+
+  //   [data],
+  // );
+
+  // const enterRects = (enter: any) => {
+  //   enter
+  //     .append('rect')
+  //     // @ts-ignore
+  //     .attr('x', (d) => xScale(d.date) || null)
+  //     .attr('width', xScale.bandwidth())
+  //     .attr('y', height - marginBottom)
+  //     .attr('height', 0)
+  //     .attr('fill', 'orange')
+  //     .selection();
+  // };
+
+  const memoizedUpdateCallback = useCallback(() => {
+    const bar = select('#wrapper');
+
+    const xMaxValue = max(data, (d) => d.value);
+
+    const xScale = scaleLinear()
+      // @ts-ignore
+      .domain([0, xMaxValue])
+      .range([marginLeft, width - marginRight]);
+
+    const yScale = scaleBand()
+      // @ts-ignore
+      .domain(data.map((d) => d.name))
+      .round(true)
+      .rangeRound([marginTop, height - marginBottom])
+      .padding(0.2);
+
+    const barSvg = bar
+      .attr('width', width)
+      .attr('height', height)
+      .attr('viewBox', [0, 0, width, height]);
+
+    const xAxis = axisBottom(xScale).ticks(10) as any;
+
+    const yAxis = (axisLeft(yScale) as any).ticks(data.length).tickSizeInner(0).tickSizeOuter(0);
+
+    barSvg.select('.y-axis').attr('transform', `translate(${marginLeft}, 0)`).call(yAxis);
+
+    barSvg
+      .select('.x-axis')
+      .attr('transform', `translate(0, ${height - marginBottom})`)
+      .transition()
+      .call(xAxis);
+
+    barSvg
+      .select('.x-axis')
+      .call(xAxis)
+      .transition()
+      .call((g) =>
+        g
+          .selectAll('.tick line')
+          .attr('stroke-opacity', 0.1)
+          .attr('y2', -height + marginTop + marginBottom),
+      );
+
+    // const rects = barSvg.selectAll('rect').data(data);
+
+    // rects
+    //   .join('rect')
+    //   .attr('class', 'bar')
+    //   .attr('x', marginLeft)
+    //   // @ts-ignore
+    //   .attr('y', (d) => yScale(d.name))
+    //   .attr('height', yScale.bandwidth())
+    //   .transition()
+    //   .duration(500)
+    //   // @ts-ignore
+    //   .attr('width', ({ value }) => xScale(value) - marginLeft)
+    //   .attr('fill', 'orange');
+
+    // rects.join(
+    //   (enter) => {
+    //     return (
+    //       enter
+    //         .append('rect')
+    //         .attr('class', 'bar')
+    //         // @ts-ignore
+    //         .attr('x', xScale(0) || null)
+    //         // @ts-ignore
+    //         .attr('y', (d) => yScale(d.name))
+    //         // @ts-ignore
+    //         .attr('width', (d) => xScale(d.value))
+    //         .attr('height', yScale.bandwidth())
+    //     );
+    //   },
+    // (update) => {
+    //   return (
+    //     update
+    //       .transition()
+    //       // @ts-ignore
+    //       .attr('x', (d) => xScale(d.name) || null)
+    //       // @ts-ignore
+    //       .attr('height', (d) => yScale(0) - yScale(d.value))
+    //       // @ts-ignore
+    //       .attr('y', (d) => yScale(d.value))
+    //       .attr('width', xScale.bandwidth())
+    //       .attr('fill', 'orange')
+    //       .selection()
+    //   );
+    // },
+    // (exit) => {
+    //   return exit
+    //     .transition()
+    //     .attr('height', 0)
+    //     .attr('y', height - marginBottom)
+    //     .remove();
+    // },
+    // );
+  }, [data, width, height, marginTop, marginRight, marginBottom, marginLeft]);
+
+  useEffect(() => {
+    // 데이터만 바뀌는 경우 true
+    if (!loaded) {
+      setLoaded(true);
+      console.log('여기');
+      memoizedDrawCallback();
+    } else {
+      console.log('저기');
+      memoizedUpdateCallback();
+    }
+  }, [loaded, memoizedDrawCallback, memoizedUpdateCallback]);
+
+  useEffect(() => {
+    // 리사이즈 일 때
+    memoizedDrawCallback();
+    memoizedUpdateCallback();
+  }, [width, height]);
+
+  return (
+    <svg
+      id="wrapper"
+      width={width}
+      height={height}
+      style={{
+        overflow: 'visible',
+        background: 'yellow',
+      }}
+    >
+      <g className="x-axis" />
+      <g className="y-axis" />
+    </svg>
+  );
+};
+
+export default HorizontalBarChart;
---
+++ apps/d3/pages/horizontal/index.tsx
@@ -0,0 +1,118 @@
+import { useEffect, useRef, useMemo, useState } from 'react';
+
+import useWindowDimensions from '../WindowDimensions';
+import HorizontalBarChart from './HorizontalBarChart';
+
+export type Data = {
+  name?: string;
+  value?: number;
+};
+
+export type Dimensions = {
+  width: number;
+  height: number;
+  margin: {
+    left: number;
+    right: number;
+    top: number;
+    bottom: number;
+  };
+  boundedWidth: number;
+  boundedHeight: number;
+};
+
+const campaign: Data[] = [
+  { name: '운영체제 최신보안 패치', value: 12 },
+  { name: '보안센터 서비스 실행', value: 14 },
+  { name: 'Windows 방화벽 사용', value: 8 },
+  { name: '윈도우 로그인 패스워드', value: 1 },
+  { name: '기타', value: 4 },
+  { name: '바이러스 백신 설치 및 실행', value: 7 },
+];
+
+export default function D3() {
+  const [data, setData] = useState<Data[]>([]);
+  const [propertiesNames] = useState(['name', 'value']);
+  const { width, height } = useWindowDimensions();
+
+  const [childrenWidth, setChildrenWidth] = useState<number>(0);
+
+  const rootRef = useRef<HTMLDivElement>(null);
+  const childrenRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (childrenRef.current) {
+      setChildrenWidth(childrenRef.current.offsetWidth);
+    }
+  }, [childrenRef, width]);
+
+  const getDimensions = useMemo(
+    () =>
+      (width: number, height: number, top: number, right: number, bottom: number, left: number) => {
+        const dimensions = {
+          width,
+          height,
+          margin: {
+            top,
+            right,
+            bottom,
+            left,
+          },
+          boundedWidth: 0,
+          boundedHeight: 0,
+        };
+        dimensions.boundedWidth =
+          dimensions.width - dimensions.margin.left - dimensions.margin.right;
+        dimensions.boundedHeight =
+          dimensions.height - dimensions.margin.top - dimensions.margin.bottom;
+
+        return dimensions;
+      },
+    [width, height],
+  );
+
+  const dimensions = useRef() as { current: Dimensions };
+  dimensions.current = getDimensions(childrenWidth * 1, 400, 20, 10, 30, 50);
+
+  // console.log('width', width);
+  // console.log('dimensions.current', dimensions.current.width);
+
+  // resize
+  useEffect(() => {
+    (dimensions as unknown as { current: Dimensions }).current = getDimensions(
+      childrenWidth * 1,
+      400 * 1,
+      20,
+      10,
+      30,
+      50,
+    );
+  }, [childrenRef, width, height, dimensions]);
+
+  useEffect(() => {
+    setData(campaign as unknown as Data[]);
+  }, [campaign]);
+
+  return (
+    <div ref={rootRef} className="grid grid-flow-col grid-rows-2 grid-cols-2 gap-8">
+      <div ref={childrenRef}>
+        test
+        {data.length > 1 ? (
+          <>
+            <h3>Bar Chart, Horizontal</h3>
+            <HorizontalBarChart
+              dimensions={dimensions.current}
+              data={data}
+              propertiesNames={propertiesNames}
+            />
+          </>
+        ) : (
+          <>Loading</>
+        )}
+      </div>
+      <div>3</div>
+      <div>2</div>
+      <div>4</div>
+    </div>
+  );
+}
